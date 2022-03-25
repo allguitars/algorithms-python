c78ba4d6694a87f9582c2209b216b8c602e662b7
Dave Lee <isho.lee@gmail.com>
2022-03-26 00:34:52 +0800

    add bubble sort demo

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,26 @@
+# https://www.youtube.com/watch?v=g_xesqdQqvA&ab_channel=DerrickSherrill
+
+def bubble_sort(list_a):
+    indexing_legth = len(list_a) - 1
+    is_sorted = False
+    print(list_a)
+
+    while not is_sorted:
+        is_sorted = True
+
+        for i in range(0, indexing_legth):
+            print(f'Comparing item[{i}]({list_a[i]}) and item[{i+1}]({list_a[i+1]})')
+
+            if list_a[i] > list_a[i+1]:
+                is_sorted = False
+                list_a[i], list_a[i + 1] = list_a[i + 1], list_a[i]
+
+            print(list_a)
+
+    return list_a
+
+
+list_in = [8, 3, 5, 10, 7]
+
+result = bubble_sort(list_in)
+print(result)
