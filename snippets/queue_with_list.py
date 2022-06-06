969d8bec8238c19b6079826fb0968576b48ed32e
Dave Lee <isho.lee@gmail.com>
2022-06-06 15:58:54 +0800

    implement a queue with list

---
+++ snippets/queue_with_list.py
@@ -0,0 +1,20 @@
+mylist = []
+
+# enqueue using append()
+mylist.append('a')
+print(mylist)
+mylist.append('b')
+print(mylist)
+mylist.append('c')
+print(mylist)
+
+# dequeue using pop()
+front = mylist.pop(0)
+print(front)
+print(mylist)
+front = mylist.pop(0)
+print(front)
+print(mylist)
+front = mylist.pop(0)
+print(front)
+print(mylist)
