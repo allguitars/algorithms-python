5c879dce330ed4e61b788becb51e8eb6749e2ddb
Dave Lee <isho.lee@gmail.com>
2022-06-12 03:37:15 +0800

    add complexity explanation

--- trees/binary_tree_operation/04_maximum_depth_recursively.py
+++ trees/binary_tree_operation/04_maximum_depth_recursively.py
@@ -2,10 +2,20 @@
 https://favtutor.com/blogs/binary-tree-height
 
 LeetCode 上的定義：
-A binary tree's maximum depth is the number of 
-nodes along the longest path from the root node down to the farthest leaf node.
+A binary tree's maximum depth is the number of nodes along the longest
+path from the root node down to the farthest leaf node.
 
 利用 Recursion 求得樹高是 DFS 的概念
+
+Time:
+The time complexity of the algorithm is O(n) as we iterate through node of
+the binary tree calculating the height of the binary tree only once.
+
+Space:
+And the space complexity is also O(n) as we are following recursion, where
+recursive stack can have up to n elements.
+
+Where n is the number of nodes in the binary tree.
 '''
 
 
