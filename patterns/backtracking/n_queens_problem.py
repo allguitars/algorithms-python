cac480a42d9d3f640950f0580918f6af9b25ff68
Dave Lee <isho.lee@gmail.com>
2022-07-06 03:28:22 +0800

    rename variables

--- patterns/backtracking/n_queens_problem.py
+++ patterns/backtracking/n_queens_problem.py
@@ -19,43 +19,46 @@ There are four columns for 4 queens
 results = []
 
 
-def solution(n):
-    cols_pool = [i for i in range(n)]
+def solution(num):
+    column_pool = list(range(num))  # [0,1,2,3]
 
-    path = []  # store all decisions along the path
-    n_queens(path, cols_pool)
+    decision_path = []  # to store decisions up to the current node
+
+    n_queens(num, decision_path, column_pool)
 
 
-def n_queens(path, pool):
+def n_queens(num, current_path, current_pool):
 
-    if len(path) > 1:   # path has at least two decisions / cols
-        if is_diagonal(path[-1], path[-2]):   # pick the last two decisions
+    # path which has at least two decisions / cols
+    if len(current_path) > 1:
+        # compare the last two decisions, check if they are at the same diagonal
+        if is_diagonal(current_path[-1], current_path[-2]):
             # kill this path and then go back
             return
 
-        # if the last two decisions do not form a diagonal, then
-        # check if we have reached the end of the path
-        if len(path) == 4:
-            print(path)
-            results.append(path)
+        # if they do not form a diagonal, then
+        # check if we have reached the end node
+        if len(current_path) == num:
+            print(current_path)
+            results.append(current_path)
             return
 
     # back up the path
-    path_backup = path[:]
+    path_backup = current_path[:]
 
     # back up the pool (remaining cols)
-    pool_backup = pool[:]
+    pool_backup = current_pool[:]
 
-    for i in range(len(pool)):  # pool=[0,2] path=[1,3]
-        choice = pool.pop(i)    # choice=[0] pool=[2]
-        path.append(choice)     # [1,3,0] -> safe!
+    for i in range(len(current_pool)):  # pool=[0,2] path=[1,3]
+        choice = current_pool.pop(i)    # choice=[0] pool=[2]
+        current_path.append(choice)     # [1,3,0] -> safe!
 
-        n_queens(path, pool)
+        n_queens(num, current_path, current_pool)
 
         # before moving on to the next col, we need restore
         # the path and pool.
-        path = path_backup[:]
-        pool = pool_backup[:]
+        current_path = path_backup[:]
+        current_pool = pool_backup[:]
 
 
 def is_diagonal(first_col, second_col):
