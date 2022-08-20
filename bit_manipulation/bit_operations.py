8bb5ebddb5a6cca139d754f268547128a001e497
Dave Lee <isho.lee@gmail.com>
2022-08-20 18:22:22 +0800

    add more bit operators demo

--- bit_manipulation/bit_operations.py
+++ bit_manipulation/bit_operations.py
@@ -50,3 +50,29 @@ print('7 << 1:', 7 << 1)  # 111 -> 1110: 14
 # >>
 
 print('4 >> 2:', 4 >> 2)  # 100 -> 001: 1
+
+# ~ <- bitwise NOT: returns one’s complement
+
+print('~1:', ~1)  # -(0001 + 1) = -(0010) = -2
+print('~10:', ~10)  # ~1010 = -(1010 + 1) = -1011 = -11
+
+# ^ <- bitwise XOR
+
+print('10 ^ 4:', 10 ^ 4)  # 1010 XOR 0100 = 1110 = 14
+
+
+# count set bits of an integer
+# assume integers are represented using 32 bits
+num = 75
+print(bin(num).replace('0b', ''))  # 1001011: 4 1s and 28 0s
+
+k1 = 0  # count of set bits
+k0 = 0  # count of non-set bits
+
+for i in range(0, 32):
+    if num & (1 << i):
+        k1 += 1
+    else:
+        k0 += 1
+
+print(f'{k1} 1s and {k0} 0s')
