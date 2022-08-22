0c6318aef865f1a9680e4c87620ced7a3e337542
Dave Lee <isho.lee@gmail.com>
2022-08-22 15:31:33 +0800

    add octal decimal conversion

--- bit_manipulation/decimal_binary_conversion.py
+++ bit_manipulation/decimal_binary_conversion.py
@@ -1,11 +1,11 @@
-d = 6
+decimal_num = 6
+binary_of_decimal = bin(decimal_num).replace('0b', '')
+print(f'binary_of_decimal {decimal_num}: {binary_of_decimal}')  # 110
 
-a = bin(d).replace('0b', '')
+binary_num = '1000'
+decimal_of_binary = int(binary_num, 2)
+print(f'decimal_of_binary {binary_num}: {decimal_of_binary}')  # 8
 
-print(a)  # 110
-
-a2 = '1000'
-
-d2 = int(a2, 2)
-
-print(d2)  # 8
+octal_num = '13'
+decimal_of_octal = int(octal_num, 8)
+print(f'decimal_of_octal {octal_num}: {decimal_of_octal}')
