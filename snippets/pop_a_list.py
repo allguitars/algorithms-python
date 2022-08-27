7435c3c8ae6f08aa10ba8499fadbbda0168af48b
Dave Lee <isho.lee@gmail.com>
2022-08-28 02:19:42 +0800

    add pop list snippet

---
+++ snippets/pop_a_list.py
@@ -0,0 +1,13 @@
+lst = [1, 2, 3, 4]
+lst2 = []
+
+# pop and assign to a variable
+n = lst.pop()
+
+print(lst)
+print(n)
+
+# pop and append to another list
+lst2.append(lst.pop())
+print(lst)
+print(lst2)
